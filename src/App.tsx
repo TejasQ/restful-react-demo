2edd9112ae9f6b98e44db34289437d16d0c2de5c
Tejas Kumar <tejas@contiamo.com>
2019-11-22 19:31:23 +0100

    It's all working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useTrendingStickers } from "./api"
 
 const App = () => {
+  const { error, loading, data: gifResponse } = useTrendingStickers({})
+
   return (
     <div>
       <h1>😍 Hello REACTCONF! ⚛️</h1>
+      {error && <h1>{error.message}</h1>}
+      {loading && <h2>Loading...</h2>}
+      {gifResponse && gifResponse.data?.map(gif => <img key={gif.id} alt={gif.slug} src={gif.images?.original?.url} />)}
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
+import { RestfulProvider } from "restful-react";
 
 import App from "./App";
 
-render(<App />, document.querySelector("#app"));
+render(
+  <RestfulProvider queryParams={{ apiKey: "kzbskSGIM5KqFH05L9WrSskLQsITRWbb" }} base="https://api.giphy.com/v1">
+    <App />
+  </RestfulProvider>,
+  document.querySelector("#app"),
+);
